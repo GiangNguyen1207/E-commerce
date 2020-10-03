7a98df8dc05f54f78a2bfb93395ab3cd5a2b2117
giang <nguyengiang.nchg@gmail.com>
2020-10-03 09:33:51 +0300

    modify hide and show password

--- front_end/src/pages/auth/components/resetPassword/index.tsx
+++ front_end/src/pages/auth/components/resetPassword/index.tsx
@@ -5,10 +5,15 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
 import LockIcon from '@material-ui/icons/Lock';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import IconButton from '@material-ui/core/IconButton';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 import useAuth from 'pages/auth/hooks/useAuth'
 import { resetPassword } from 'pages/auth/redux/actions'
@@ -28,9 +33,11 @@ const useStyles = makeStyles((theme: Theme) =>
       textAlign: 'center'
     },
     form: {
-      marginTop: '40px',
       display: 'flex',
       flexDirection: 'column',
+    },
+    passwords: {
+      marginTop: '40px'
     },
     button: {
       marginTop: '40px',
@@ -44,8 +51,20 @@ const ResetPassword = () => {
   const classes = useStyles();
   const dispatch = useDispatch()
   const { user } = useAuth()
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false)
+  const [showNewPassword, setShowNewPassword] = useState(false)
   const [oldPassword, setOldPassword] = useState('')
   const [newPassword, setNewPassword] = useState('')
+
+  const passwords = ['Current password', 'New password']
+
+  const handleClickShowCurrentPassword = () => {
+    setShowCurrentPassword(!showCurrentPassword);
+  };
+
+  const handleClickShowNewPassword = () => {
+    setShowNewPassword(!showNewPassword);
+  };
 
   const onChangeOldPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setOldPassword(event.target.value)
@@ -70,19 +89,40 @@ const ResetPassword = () => {
           Reset your password
         </Typography>
         <form className={classes.form} onSubmit={onSubmit}>
-          <TextField
-            onChange={onChangeOldPassword}
-            id="filled-read-only-old-password"
-            label='Current password'
-            variant="outlined"
-            />
-          <TextField
-            onChange={onChangeNewPassword}
-            id="filled-read-only-new-password"
-            label='New password'
-            variant="outlined"
-            style={{marginTop: '40px'}}
-          />
+          {passwords.map(p => {
+            return(
+              <div className={classes.passwords}>
+                <InputLabel htmlFor="passwords">{p}</InputLabel>
+                <OutlinedInput
+                  id="-passwords"
+                  style={{marginTop: '10px', width: '100%'}}
+                  type={p === 'Current password' ? 
+                    showCurrentPassword ? 'text' : 'password' 
+                    : showNewPassword ? 'text' : 'password'
+                  }
+                  onChange={p === 'Current password' ? 
+                    onChangeOldPassword :
+                    onChangeNewPassword
+                  }
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={p === 'Current password' ? 
+                          handleClickShowCurrentPassword 
+                          : handleClickShowNewPassword
+                        }>
+                        {p === 'Current password' ? 
+                          showCurrentPassword ? <Visibility /> : <VisibilityOff />
+                          : showNewPassword ? <Visibility /> : <VisibilityOff />
+                        }
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </div>
+            )
+          })}
           <Button
             type='submit'
             size='large'
